24ca7c98e0fb65ea4df42af689275f9ce9f9eeae
KirillovE <evgeniy.kirillov@live.ru>
2018-02-16 23:01:05 +0300

    Добавил элементы на главный экран

--- newVK/MainVC.swift
+++ newVK/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  newVK
 //
 //  Created by Евгений Кириллов on 16.02.2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
