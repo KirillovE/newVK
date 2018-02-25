29e53bc8585a1ed15e9b3f5f6794252189ba8090
KirillovE <evgeniy.kirillov@live.ru>
2018-02-25 16:54:48 +0300

    Готовы все группы

--- newVK/AllGroupsTableVC.swift
+++ newVK/AllGroupsTableVC.swift
@@ -9,87 +9,49 @@
 import UIKit
 
 class AllGroupsTableVC: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    // MARK: - Source data
+    
+    struct Group {
+        let name, imageName: String
+        let subscriberCount: Int
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    var allGroups: [Group] = [Group(name: "ГикБрейнс", imageName: "группа.гикбрейнс", subscriberCount: 10),
+                             Group(name: "Кино", imageName: "группа.кино", subscriberCount: 20),
+                             Group(name: "НЛО", imageName: "группа.нло", subscriberCount: 1),
+                             Group(name: "Swift", imageName: "группа.свифт", subscriberCount: 77),
+                             Group(name: "Тёмная сторона", imageName: "группа.тёмная сторона", subscriberCount: 2),
+                             Group(name: "Tesla", imageName: "группа.тесла", subscriberCount: 300),
+                             Group(name: "Формула-1", imageName: "группа.формула 1", subscriberCount: 100),
+                             Group(name: "Формула-E", imageName: "группа.формула е", subscriberCount: 50),
+                             Group(name: "Apple", imageName: "группа.эпл", subscriberCount: 700),
+                             Group(name: "Linux", imageName: "группа.Linux", subscriberCount: 15),
+                             Group(name: "Objective-C", imageName: "группа.Oblective C", subscriberCount: 2),
+                             Group(name: "Windows", imageName: "группа.Windows", subscriberCount: 99)
+    ]
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return allGroups.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "allGroups", for: indexPath)
 
-        // Configure the cell...
+        let groupName = allGroups[indexPath.row].name
+        let groupImageName = allGroups[indexPath.row].imageName
+        let groupSubscriberCount = allGroups[indexPath.row].subscriberCount
+        
+        cell.textLabel?.text = groupName
+        cell.imageView?.image = UIImage(named: groupImageName)
+        cell.detailTextLabel?.text = String(groupSubscriberCount)
+        
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 4
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
