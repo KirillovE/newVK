6f174c16b8eb1a6e19d67179b7de7f81536e7b65
KirillovE <evgeniy.kirillov@live.ru>
2018-02-27 22:02:21 +0300

    Revert "Фотографии теперь масштабируются"
    This reverts commit 38e36a00f4f07f7ae6d5f68b2d81d73e624024b1.

--- newVK/LargePhotoVC.swift
+++ newVK/LargePhotoVC.swift
@@ -12,11 +12,6 @@ class LargePhotoVC: UIViewController {
     
 // MARK: - Variables
     
-    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var largePhoto: UIImageView!
     var photoName: String?
     
@@ -30,22 +25,6 @@ class LargePhotoVC: UIViewController {
         
         let viewTapGesture = UITapGestureRecognizer(target: self, action: #selector(onTap))
         view.addGestureRecognizer(viewTapGesture)
-        
-        updateConstraintsForSize(view.bounds.size)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        updateMinZoomScaleForSize(view.bounds.size)
-    }
-    
-    fileprivate func updateMinZoomScaleForSize(_ size: CGSize) {
-        let widthScale = size.width / largePhoto.bounds.width
-        let heightScale = size.height / largePhoto.bounds.height
-        let minScale = min(widthScale, heightScale)
-        
-        scrollView.minimumZoomScale = minScale
-        scrollView.zoomScale = minScale
     }
     
 // MARK: - Other methods
@@ -74,32 +53,6 @@ class LargePhotoVC: UIViewController {
         default:
             navigationController?.setNavigationBarHidden(true, animated: true)
         }
-    }
-    
-}
-
-// MARK: - Extensions
-
-extension LargePhotoVC: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return largePhoto
-    }
-    
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        updateConstraintsForSize(view.bounds.size)
-    }
-    
-    fileprivate func updateConstraintsForSize(_ size: CGSize) {
-        
-        let yOffset = max(0, (size.height - largePhoto.frame.height) / 2)
-        imageViewTopConstraint.constant = yOffset
-        imageViewBottomConstraint.constant = yOffset
-        
-        let xOffset = max(0, (size.width - largePhoto.frame.width) / 2)
-        imageViewLeadingConstraint.constant = xOffset
-        imageViewTrailingConstraint.constant = xOffset
-        
-        view.layoutIfNeeded()
     }
     
 }
