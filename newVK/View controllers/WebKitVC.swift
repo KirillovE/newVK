c908c42519f0d3b96686985b7c29ec593787876f
KirillovE <evgeniy.kirillov@live.ru>
2018-03-03 16:03:27 +0300

    Добавлен класс WebKit view controller

---
+++ newVK/View controllers/WebKitVC.swift
@@ -0,0 +1,35 @@
+//
+//  WebKitVC.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 03.03.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import WebKit
+
+class WebKitVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
