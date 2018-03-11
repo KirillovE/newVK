fe426000a25cd41a4170e791d40e749f76f3295f
KirillovE <evgeniy.kirillov@live.ru>
2018-03-11 23:36:04 +0300

    Отображаются имена друзей

--- newVK/View controllers/FriendsVC.swift
+++ newVK/View controllers/FriendsVC.swift
@@ -7,66 +7,88 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class FriendsVC: UITableViewController {
 
 // MARK: - Source data
 
     var settings: SettingsStorage!
-    
-    struct Friend {
-        let name: String
-        let imageName: String
+    let vkRequest = VKRequestService()
+    var friends = [User]()
+    var friendsJSON: JSON? {
+        didSet {
+            friends = appendFriends(from: friendsJSON)
+            self.tableView.reloadData()
+        }
     }
-    
-    let friendsList: [Friend] = [Friend(name: "Адриана Лима", imageName: "друг.Адриана"),
-                                 Friend(name: "Алессандра Амбросио", imageName: "друг.Алессандра"),
-                                 Friend(name: "Дэвид Бекхэм", imageName: "друг.Бекхэм"),
-                                 Friend(name: "В", imageName: "друг.В"),
-                                 Friend(name: "Леонардо Ди Каприо", imageName: "друг.Ди Каприо"),
-                                 Friend(name: "Павел Дуров", imageName: "друг.Дуров"),
-                                 Friend(name: "Илон Маск", imageName: "друг.Маск"),
-                                 Friend(name: "Меган Фокс", imageName: "друг.Меган"),
-                                 Friend(name: "Нео", imageName: "друг.Нео"),
-                                 Friend(name: "Криштиану Роналду", imageName: "друг.Роналду"),
-                                 Friend(name: "Роузи Хантингтон-Уайтли", imageName: "друг.Роузи"),
-                                 Friend(name: "Энакин Скайуокер", imageName: "друг.Энакин")
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(settings.accessToken, settings.userID, settings.apiVersion)
+        getFriends()
     }
     
 // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendsList.count
+        return friends.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Friends", for: indexPath)
         
-        let friendName = friendsList[indexPath.row].name
-        let friendImageName = friendsList[indexPath.row].imageName
+        let firstName = friends[indexPath.row].firstName
+        let lastName = friends[indexPath.row].lastName
+        cell.textLabel?.text = firstName
+        cell.detailTextLabel?.text = lastName
         
-        cell.textLabel?.text = friendName
-        cell.imageView?.image = UIImage.init(named: friendImageName)
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+//        пока есть только адрес фотографии, сам файл предстоит получить
+//        let friendImageName = friendsList[indexPath.row].imageName
+//        cell.imageView?.image = UIImage.init(named: friendImageName)
+//        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+//        cell.imageView?.clipsToBounds = true
         return cell
     }
 
 // MARK: - Navigation
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowFriendImage" {
-            let cell = sender as! UITableViewCell
-            let imageIndex = self.tableView.indexPath(for: cell)?.row
-            let collectionVC = segue.destination as! FriendPhotosVC
-            collectionVC.albumName = friendsList[imageIndex!].imageName
-            collectionVC.title = friendsList[imageIndex!].name
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "ShowFriendImage" {
+//            let cell = sender as! UITableViewCell
+//            let imageIndex = self.tableView.indexPath(for: cell)?.row
+//            let collectionVC = segue.destination as! FriendPhotosVC
+//            collectionVC.albumName = friendsList[imageIndex!].imageName
+//            collectionVC.title = friendsList[imageIndex!].name
+//        }
+//    }
+}
+
+// MARK: - Requesting friends from server
+
+extension FriendsVC {
+    func getFriends() {
+        let parameters: Parameters = ["fields": "nickName",
+                                      "access_token": settings.accessToken,
+                                      "v": settings.apiVersion
+        ]
+        
+        vkRequest.makeRequest(method: "friends.get", parameters: parameters) { [weak self] json in
+            self?.friendsJSON = json
         }
-    }    
+    }
+    
+    func appendFriends(from json: JSON?) -> [User] {
+        guard json != nil else { return [User]() }
+        
+        let itemsArray = json!["response", "items"]
+        var friendsArray = [User]()
+        
+        for (_, item) in itemsArray {
+            let user = User(json: item)
+            friendsArray.append(user)
+        }
+        
+        return friendsArray
+    }
 }
