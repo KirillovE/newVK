fd6f53e037c94617c578abb2dc61f64355f89914
KirillovE <evgeniy.kirillov@live.ru>
2018-03-11 22:33:20 +0300

    Отображаются настоящие группы в соответствующей таблице. Пока без изображений, без возможности удаления и добавления

--- newVK/View controllers/MyGroupsVC.swift
+++ newVK/View controllers/MyGroupsVC.swift
@@ -15,34 +15,43 @@ class MyGroupsVC: UITableViewController {
 // MARK: - Source data
     
     var settings: SettingsStorage!
+    let vkRequest = VKRequestService()
+    var groups = [Group]()
+    var groupsJSON: JSON? {
+        didSet {
+            groups = appendGroups(from: groupsJSON)
+            self.tableView.reloadData()
+        }
+    }
 
     var myGroups: [AllGroupsVC.Group] =
         [AllGroupsVC.Group(name: "ГикБрейнс", imageName: "группа.гикбрейнс", subscriberCount: 10),
          AllGroupsVC.Group(name: "Swift", imageName: "группа.свифт", subscriberCount: 77)
         ]
     
+    // MARK: - View Controller life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(settings.accessToken, settings.userID, settings.apiVersion)
+        getGroups()
     }
 
 // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myGroups.count
+        return groups.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myGroups", for: indexPath)
-        let groupName = myGroups[indexPath.row].name
-        let groupImageName = myGroups[indexPath.row].imageName
+        let groupName = groups[indexPath.row].name
+//        let groupImageName = myGroups[indexPath.row].imageName
         
         cell.textLabel?.text = groupName
-        cell.imageView?.image = UIImage(named: groupImageName)
+//        cell.imageView?.image = UIImage(named: groupImageName)
         
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 4
-        cell.imageView?.clipsToBounds = true
+//        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 4
+//        cell.imageView?.clipsToBounds = true
 
         return cell
     }
@@ -59,6 +68,8 @@ class MyGroupsVC: UITableViewController {
 
 extension MyGroupsVC {
 
+    // MARK: - Adding a group
+    
     @IBAction func addGroup(segue: UIStoryboardSegue) {
         guard segue.identifier == "addGroupSegue" else { return }
         let allGroupsVC = segue.source as! AllGroupsVC
@@ -77,6 +88,45 @@ extension MyGroupsVC {
             }
         }
         
+    }
+    
+    // MARK: - Requesting groups from server
+    
+    func getGroups() {
+        let parameters: Parameters = ["user_id": settings.userID,
+                                      "extended": 1,
+                                      "access_token": settings.accessToken,
+                                      "v": settings.apiVersion
+        ]
+        
+        vkRequest.makeRequest(method: "groups.get", parameters: parameters) { [weak self] json in
+            self?.groupsJSON = json
+        }
+    }
+    
+    func getSearchedGroups(groupToFind q: String, numberOfResults: Int) {
+        let parameters: Parameters = ["q": q,
+                                      "type": "group",
+                                      "count": numberOfResults,
+                                      "access_token": settings.accessToken,
+                                      "v": settings.apiVersion
+        ]
+        
+        vkRequest.makeRequest(method: "groups.search", parameters: parameters) { [weak self] json in
+            self?.groupsJSON = json
+        }
+    }
+    
+    func appendGroups(from json: JSON?) -> [Group] {
+        let itemsArray = json!["response", "items"]
+        var groupsArray = [Group]()
+        
+        for (_, item) in itemsArray {
+            let group = Group(json: item)
+            groupsArray.append(group)
+        }
+        
+        return groupsArray
     }
     
 }
