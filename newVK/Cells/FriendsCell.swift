1a5db2be7c11c1b8cc987eab49df347267b9ee30
KirillovE <evgeniy.kirillov@live.ru>
2018-03-21 17:22:40 +0300

    Для друзей загружаются фотографии-аватары

--- newVK/Cells/FriendsCell.swift
+++ newVK/Cells/FriendsCell.swift
@@ -9,18 +9,30 @@
 import UIKit
 
 class FriendsCell: UITableViewCell {
+    
     func configure(for friend: User) {
         
         let firstName = friend.firstName
         let lastName = friend.lastName
         let nick = friend.nick
-        let avatar = friend.avatar
+        loadPhoto(from: friend.avatarURL)
         
         textLabel?.text = firstName + " " + lastName
         detailTextLabel?.text = nick
-        imageView?.image = avatar
         
         imageView?.layer.cornerRadius = frame.size.height / 2
         imageView?.clipsToBounds = true
     }
+    
+    func loadPhoto(from urlString: String) {
+        let url = URL(string: urlString)
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url!) else { return }
+            DispatchQueue.main.async {
+                guard let image = UIImage(data: data) else { return }
+                self.imageView?.image = image
+            }
+        }
+    }
+
 }
