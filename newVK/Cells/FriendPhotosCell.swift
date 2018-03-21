a9ec10ad884b02bce37b9552f751febb3d931842
KirillovE <evgeniy.kirillov@live.ru>
2018-03-21 17:34:32 +0300

    Загружаются фотографии по полученному адресу

--- newVK/Cells/FriendPhotosCell.swift
+++ newVK/Cells/FriendPhotosCell.swift
@@ -13,12 +13,21 @@ class FriendPhotosCell: UICollectionViewCell {
     @IBOutlet weak var friendImage: UIImageView!
     
     func configure(for photo: Photo) {
-        
-        let photo = photo.largePhoto
-        friendImage.image = photo
+        loadPhoto(from: photo.smallPhotoURL)
         
         friendImage.layer.cornerRadius = frame.size.height / 10
         friendImage.clipsToBounds = true
+    }
+    
+    func loadPhoto(from urlString: String) {
+        let url = URL(string: urlString)
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url!) else { return }
+            DispatchQueue.main.async {
+                guard let image = UIImage(data: data) else { return }
+                self.friendImage.image = image
+            }
+        }
     }
     
 }
