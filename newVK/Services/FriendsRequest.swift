7070736c8d2d8b47be71acb2778ca7715529c194
KirillovE <evgeniy.kirillov@live.ru>
2018-03-21 10:06:48 +0300

    Создан отдельный класс для выполнения запроса и заполнения массива друзей

---
+++ newVK/Services/FriendsRequest.swift
@@ -0,0 +1,69 @@
+//
+//  FriendsRequest.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 03.03.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+import SwiftKeychainWrapper
+
+class FriendsRequest {
+    
+    var sessionManager: SessionManager?
+    let method = "friends.get"
+    let requestFields = "nickName,photo_100"
+    
+    func makeRequest() {
+        let (accessToken, apiVersion, url) = configureRequest()
+        
+        let parameters: Parameters = ["fields": requestFields,
+                                      "access_token": accessToken,
+                                      "v": apiVersion
+        ]
+        
+        sessionManager?.request(url! + method, parameters: parameters).responseJSON {[weak self] response in
+            guard let data = response.value else {return}
+            let json = JSON(data)
+            let friends = self?.appendFriends(json: json)
+            let saving = SavingObjects()
+            saving.save(objectsArray: friends!)
+        }
+    }
+    
+    /// подготавливает настройки для запроса
+    func configureRequest() -> (String, Double, String?) {
+        let config = URLSessionConfiguration.default
+        config.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        sessionManager = SessionManager(configuration: config)
+        
+        let accessToken = KeychainWrapper.standard.string(forKey: "access_token")!
+        let userDefaults = UserDefaults.standard
+        let apiVersion = userDefaults.double(forKey: "v")
+        let url = userDefaults.string(forKey: "apiURL")
+        
+        return (accessToken, apiVersion, url ?? "")
+    }
+    
+    /// выдаёт массив друзей
+    func appendFriends(json: JSON) -> [User] {
+        guard json["error", "error_code"] != 5 else {
+            print("не подошёл access_token ", json["error", "error_msg"])
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(false, forKey: "isAuthorized")
+            return [User]()
+        }
+        
+        let itemsArray = json["response", "items"]
+        var friendsArray = [User]()
+        for (_, item) in itemsArray {
+            let user = User(json: item)
+            friendsArray.append(user)
+        }
+        
+        return friendsArray
+    }
+
+}
