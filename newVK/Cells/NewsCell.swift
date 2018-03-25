64c6d89ca5552215f7489a9f8f72aad6b91a733e
KirillovE <evgeniy.kirillov@live.ru>
2018-03-25 11:45:05 +0300

    Созданы классы для таблицы и ячейки новостей

---
+++ newVK/Cells/NewsCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCell.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 25.03.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+
+}
---
+++ newVK/View controllers for news/NewsVC.swift
@@ -0,0 +1,32 @@
+//
+//  NewsVC.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 25.03.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import UIKit
+
+class NewsVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
