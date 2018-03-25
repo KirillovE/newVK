bf0208876d5ba871a389fcc982161ce5fa7343c8
KirillovE <evgeniy.kirillov@live.ru>
2018-03-25 13:01:18 +0300

    В ленте новостей отображаются демонстрационные данные

--- newVK/Cells/NewsCell.swift
+++ newVK/Cells/NewsCell.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class NewsCell: UITableViewCell {
-
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var newsText: UILabel!
+    @IBOutlet weak var numberOfViews: UILabel!
+    @IBOutlet weak var numberOfReposts: UILabel!
+    @IBOutlet weak var numberOfComments: UILabel!
+    @IBOutlet weak var numberOfLikes: UILabel!
+    
+    // MARK:
+    
+    func configure(for news: NewsVC.News) {
+        avatar.image = news.avatar
+        authorName.text = news.name
+        newsText.text = news.text
+        numberOfViews.text = String(news.views)
+        numberOfReposts.text = String(news.reposts)
+        numberOfComments.text = String(news.comments)
+        numberOfLikes.text = String(news.likes)
+        
+        avatar.layer.cornerRadius = avatar.frame.size.height / 2
+        avatar.clipsToBounds = true
+    }
 }
--- newVK/View controllers for news/NewsVC.swift
+++ newVK/View controllers for news/NewsVC.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class NewsVC: UITableViewController {
 
+    // MARK: - Demo data
+    
+    struct News {
+        let avatar: UIImage
+        let name, text: String
+        let views, reposts, comments, likes: Int
+    }
+    
+    let blockTelegram = News(avatar: #imageLiteral(resourceName: "цитаты"),
+                             name: "IT новости",
+                             text: "Роскомнадзор обязал Telegram в течение 15 дней передать ключи для шифрования переписок. Если требование не будет исполнено, ведомство может начать блокировку мессенджера на территории РФ",
+                             views: 500,
+                             reposts: 10,
+                             comments: 333,
+                             likes: 0)
+    let answerOfTelegram = News(avatar: #imageLiteral(resourceName: "цитаты"),
+                                name: "IT новости",
+                                text: "Telegram оспорил в ЕСПЧ штраф за отказ раскрыть ФСБ ключи шифрования. Компания Telegram Messenger направила в Европейский суд по правам человека (ЕСПЧ) жалобу на решение Мещанского суда, оштрафовавшего ее на 800 000 руб. за отказ предоставить ФСБ ключи шифрования сообщений пользователей",
+                                views: 670,
+                                reposts: 15,
+                                comments: 572,
+                                likes: 1_000)
+    var newsFeed = [News]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newsFeed = [blockTelegram, answerOfTelegram]
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return newsFeed.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "newsCell", for: indexPath) as! NewsCell
 
-        // Configure the cell...
+        cell.configure(for: newsFeed[indexPath.row])
 
         return cell
     }
