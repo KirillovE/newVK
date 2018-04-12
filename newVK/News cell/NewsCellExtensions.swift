c65dcaf2889a76d02c4c51663b3c2312335f586c
KirillovE <evgeniy.kirillov@live.ru>
2018-04-12 15:17:36 +0300

    Удалён лишний пробел

--- newVK/News cell/NewsCellExtensions.swift
+++ newVK/News cell/NewsCellExtensions.swift
@@ -13,7 +13,7 @@ extension NewsCell {
     func getLabelSize(text: String, font: UIFont) -> CGSize {
         let maxWidth = bounds.width - inset * 2
         let textBlock = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
-        let rect = text.boundingRect(with: textBlock, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font : font], context: nil)
+        let rect = text.boundingRect(with: textBlock, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
         let width = Double(rect.size.width)
         let height = Double(rect.size.height)
         let size = CGSize(width: ceil(width), height: ceil(height))
