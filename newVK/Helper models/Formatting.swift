6c585279208ea654fb7142bd649a94ce816a3a22
KirillovE <evgeniy.kirillov@live.ru>
2018-04-12 11:28:53 +0300

    Класс теперь может форматировать дату

---
+++ newVK/Helper models/Formatting.swift
@@ -0,0 +1,45 @@
+//
+//  IntFormatting.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 12.04.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import Foundation
+
+class Formatting {
+    
+    enum DateOutputFormat: String {
+        case day = "dd.MM.yyyy"
+        case time = "HH.mm"
+    }
+    
+    private let intFormatter: NumberFormatter = {
+        let fmtr = NumberFormatter()
+        fmtr.usesGroupingSeparator = true
+        fmtr.numberStyle = .decimal
+        return fmtr
+    }()
+    
+    private let dateFormatter = DateFormatter()
+    
+    func formatInt(_ number: Int) -> String {
+        let niceNumber = intFormatter.string(from: NSNumber(integerLiteral: number))
+        
+        if let niceString = niceNumber {
+            return niceString
+        } else {
+            return "нет данных"
+        }
+    }
+    
+    func formatDate(_ date: Double, outputFormat: DateOutputFormat) -> String {
+        dateFormatter.dateFormat = outputFormat.rawValue
+        let numberDate = Date(timeIntervalSince1970: date)
+        let formattedDate = dateFormatter.string(from: numberDate)
+        
+        return formattedDate
+    }
+    
+}
