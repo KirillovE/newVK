0a94e0f3f5d02d1e197be4f58393c469f8247623
KirillovE <evgeniy.kirillov@live.ru>
2018-04-12 16:10:09 +0300

    Удалил старый ненужный код

--- newVK/Cells/MyGroupsCell.swift
+++ newVK/Cells/MyGroupsCell.swift
@@ -11,25 +11,10 @@ import UIKit
 class MyGroupsCell: UITableViewCell {
     
     func configure(for group: Group) {
-        let groupName = group.name
-        loadPhoto(from: group.photoURL)
-        
-        textLabel?.text = groupName
+        textLabel?.text = group.name
         
         imageView?.layer.cornerRadius = frame.size.height / 4
         imageView?.clipsToBounds = true
         imageView?.backgroundColor = UIColor.white
     }
-    
-    func loadPhoto(from urlString: String) {
-        let url = URL(string: urlString)
-        DispatchQueue.global().async {
-            guard let data = try? Data(contentsOf: url!) else { return }
-            DispatchQueue.main.async {
-                guard let image = UIImage(data: data) else { return }
-                self.imageView?.image = image
-            }
-        }
-    }
-    
 }
