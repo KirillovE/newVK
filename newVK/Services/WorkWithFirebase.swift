40e0f895b2b1fcb8d3efb2ce3159a285c9d9b518
KirillovE <evgeniy.kirillov@live.ru>
2018-04-16 10:27:50 +0300

    Небольшая оптимизация класса

--- newVK/Services/WorkWithFirebase.swift
+++ newVK/Services/WorkWithFirebase.swift
@@ -11,28 +11,29 @@ import SwiftyJSON
 
 class WorkWithFirebase {
     
+    let userDefaults = UserDefaults.standard
+    
     func saveIndexOfUser(authorizedUser id: String?) {
-        let userDefaults = UserDefaults.standard
         guard let userID = id else { return }
         let ref = Database.database().reference()
         let dict = ["ID": userID]
         
-        ref.child("Users").observeSingleEvent(of: .value) { snapshot in
+        ref.child("Users").observeSingleEvent(of: .value) { [weak self] snapshot in
             let json = JSON(snapshot.value as Any)
             let usersArray = json.arrayValue
             
-            if let indexOfUser = self.indexOf(id: userID, in: usersArray) {
-                userDefaults.set(indexOfUser, forKey: "numberOfUserInFireBase")
+            if let indexOfUser = self?.indexOf(id: userID, in: usersArray) {
+                self?.userDefaults.set(indexOfUser, forKey: "numberOfUserInFireBase")
             } else {
                 let newIndex = String(usersArray.count)
                 ref.child("Users").updateChildValues([newIndex: dict])
-                userDefaults.set(newIndex, forKey: "numberOfUserInFireBase")
+                self?.userDefaults.set(newIndex, forKey: "numberOfUserInFireBase")
             }
         }
         
     }
     
-    func indexOf(id: String, in array: [JSON]) -> String? {
+    private func indexOf(id: String, in array: [JSON]) -> String? {
         for (index, userDict) in array.enumerated() {
             if id == userDict["ID"].stringValue {
                 return String(index)
@@ -43,7 +44,6 @@ class WorkWithFirebase {
     
     func addGroup(_ group: Group) {
         let groupID = String(group.id)
-        let userDefaults = UserDefaults.standard
         let numberOfUser = userDefaults.integer(forKey: "numberOfUserInFireBase")
         let ref = Database.database().reference()
         
