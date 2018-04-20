340a3d34b0d483ef932b24074970f6da63226775
KirillovE <evgeniy.kirillov@live.ru>
2018-04-20 11:17:28 +0300

    Указал атрибут "private" там, где это нужно

--- newVK/Cells/NewsCell.swift
+++ newVK/Cells/NewsCell.swift
@@ -12,26 +12,26 @@ class NewsCell: UITableViewCell {
     
     // MARK: - Outlets
     
-    @IBOutlet weak var avatar: UIImageView!
-    @IBOutlet weak var authorName: UILabel!
-    @IBOutlet weak var newsText: UILabel!
-    @IBOutlet weak var numberOfViews: UILabel!
-    @IBOutlet weak var numberOfReposts: UILabel!
-    @IBOutlet weak var numberOfComments: UILabel!
-    @IBOutlet weak var numberOfLikes: UILabel!
-    @IBOutlet weak var date: UILabel!
-    @IBOutlet weak var attachedImage: UIImageView!
+    @IBOutlet weak private var avatar: UIImageView!
+    @IBOutlet weak private var authorName: UILabel!
+    @IBOutlet weak private var newsText: UILabel!
+    @IBOutlet weak private var numberOfViews: UILabel!
+    @IBOutlet weak private var numberOfReposts: UILabel!
+    @IBOutlet weak private var numberOfComments: UILabel!
+    @IBOutlet weak private var numberOfLikes: UILabel!
+    @IBOutlet weak private var date: UILabel!
+    @IBOutlet weak private var attachedImage: UIImageView!
     
     // MARK: - Source data
     
-    let inset: CGFloat = 15
-    let insetBetweenObjects: CGFloat = 5
-    var nameDateOriginX: CGFloat = 0
-    let attributeImageSize = CGSize(width: 15, height: 15)
-    var attributeOriginY: CGFloat = 0
-    var attachedImageAspectRatio: Double?
+    private let inset: CGFloat = 15
+    private let insetBetweenObjects: CGFloat = 5
+    private var nameDateOriginX: CGFloat = 0
+    private let attributeImageSize = CGSize(width: 15, height: 15)
+    private var attributeOriginY: CGFloat = 0
+    private var attachedImageAspectRatio: Double?
     var cellHeight: CGFloat?
-    let formatter = Formatting()
+    private let formatter = Formatting()
     
     // MARK: - Methods
     
@@ -87,37 +87,37 @@ class NewsCell: UITableViewCell {
 
 extension NewsCell {
     
-    func setAuthorName(_ name: String) {
+    private func setAuthorName(_ name: String) {
         authorName.text = name
         authorNameFrame()
     }
     
-    func setDate(_ timeDate: String) {
+    private func setDate(_ timeDate: String) {
         date.text = timeDate
         dateFrame()
     }
     
-    func setNewsText(_ text: String) {
+    private func setNewsText(_ text: String) {
         newsText.text = text
         newsTextFrame()
     }
     
-    func setLikes(_ count: Int) {
+    private func setLikes(_ count: Int) {
         numberOfLikes.text = formatter.formatInt(count)
         likeCountFrame()
     }
     
-    func setComments(_ count: Int) {
+    private func setComments(_ count: Int) {
         numberOfComments.text = formatter.formatInt(count)
         commentsCountFrame()
     }
     
-    func setReposts(_ count: Int) {
+    private func setReposts(_ count: Int) {
         numberOfReposts.text = formatter.formatInt(count)
         repostsCountFrame()
     }
     
-    func setViews(_ count: Int) {
+    private func setViews(_ count: Int) {
         numberOfViews.text = formatter.formatInt(count)
         viewsCountFrame()
     }
@@ -128,47 +128,49 @@ extension NewsCell {
 
 extension NewsCell {
     
-    func avatarFrame() {
+    private func avatarFrame() {
         let avatarSideSize: CGFloat = 45
         let avatarSize = CGSize(width: avatarSideSize, height: avatarSideSize)
         let avatarOrigin = CGPoint(x: inset, y: inset)
         avatar.frame = CGRect(origin: avatarOrigin, size: avatarSize)
     }
     
-    func authorNameFrame() {
+    private func authorNameFrame() {
         let labelSize = getLabelSize(text: authorName.text!, font: authorName.font)
         let labelOrigin = CGPoint(x: nameDateOriginX, y: inset)
         authorName.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func dateFrame() {
+    private func dateFrame() {
         let labelSize = getLabelSize(text: date.text!, font: date.font)
         let labelY = inset + avatar.frame.height - labelSize.height
         let labelOrigin = CGPoint(x: nameDateOriginX, y: labelY)
         date.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func newsTextFrame() {
+    private func newsTextFrame() {
         let labelSize = getLabelSize(text: newsText.text!, font: newsText.font)
         let labelY = inset + avatar.frame.height + insetBetweenObjects
         let labelOrigin = CGPoint(x: inset, y: labelY)
         newsText.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func attachedImageFrame() {
+    private func attachedImageFrame() {
         let imageSize: CGSize
+        let imageViewY = newsText.frame.origin.y + newsText.frame.height + insetBetweenObjects
+        let imageViewOrigin = CGPoint(x: 0, y: imageViewY)
+        
         if let aspectRatio = attachedImageAspectRatio {
             let imageWidth = bounds.width
             let imageHeight = imageWidth / CGFloat(aspectRatio)
             imageSize = CGSize(width: imageWidth, height: imageHeight)
+            attachedImage.frame = CGRect(origin: imageViewOrigin, size: imageSize)
+            attributeOriginY = attachedImage.frame.origin.y + attachedImage.frame.height + insetBetweenObjects
         } else {
             imageSize = CGSize.zero
+            attachedImage.frame = CGRect(origin: imageViewOrigin, size: imageSize)
+            attributeOriginY = attachedImage.frame.origin.y
         }
-        let labelY = newsText.frame.origin.y + newsText.frame.height + insetBetweenObjects
-        let labelOrigin = CGPoint(x: 0, y: labelY)
-        attachedImage.frame = CGRect(origin: labelOrigin, size: imageSize)
-        
-        attributeOriginY = attachedImage.frame.origin.y + attachedImage.frame.height + insetBetweenObjects
     }
     
 }
@@ -177,51 +179,51 @@ extension NewsCell {
 
 extension NewsCell {
     
-    func likeImageFrame() {
+    private func likeImageFrame() {
         let labelOrigin = CGPoint(x: inset, y: attributeOriginY)
         viewWithTag(1)?.frame = CGRect(origin: labelOrigin, size: attributeImageSize)
     }
     
-    func likeCountFrame() {
+    private func likeCountFrame() {
         let labelSize = getLabelSize(text: numberOfLikes.text!, font: numberOfLikes.font)
         let labelX = (viewWithTag(1)?.frame.origin.x)! + (viewWithTag(1)?.frame.width)!
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         numberOfLikes.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func commentsImageFrame() {
+    private func commentsImageFrame() {
         let labelX = numberOfLikes.frame.origin.x + numberOfLikes.frame.width + insetBetweenObjects
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         viewWithTag(2)?.frame = CGRect(origin: labelOrigin, size: attributeImageSize)
     }
     
-    func commentsCountFrame() {
+    private func commentsCountFrame() {
         let labelSize = getLabelSize(text: numberOfComments.text!, font: numberOfComments.font)
         let labelX = (viewWithTag(2)?.frame.origin.x)! + (viewWithTag(2)?.frame.width)!
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         numberOfComments.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func repostsImageFrame() {
+    private func repostsImageFrame() {
         let labelX = numberOfComments.frame.origin.x + numberOfComments.frame.width + insetBetweenObjects
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         viewWithTag(3)?.frame = CGRect(origin: labelOrigin, size: attributeImageSize)
     }
     
-    func repostsCountFrame() {
+    private func repostsCountFrame() {
         let labelSize = getLabelSize(text: numberOfReposts.text!, font: numberOfReposts.font)
         let labelX = (viewWithTag(3)?.frame.origin.x)! + (viewWithTag(3)?.frame.width)!
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         numberOfReposts.frame = CGRect(origin: labelOrigin, size: labelSize)
     }
     
-    func viewsImageFrame() {
+    private func viewsImageFrame() {
         let labelX = numberOfReposts.frame.origin.x + numberOfReposts.frame.width + insetBetweenObjects
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
         viewWithTag(4)?.frame = CGRect(origin: labelOrigin, size: attributeImageSize)
     }
     
-    func viewsCountFrame() {
+    private func viewsCountFrame() {
         let labelSize = getLabelSize(text: numberOfViews.text!, font: numberOfViews.font)
         let labelX = (viewWithTag(4)?.frame.origin.x)! + (viewWithTag(4)?.frame.width)!
         let labelOrigin = CGPoint(x: labelX, y: attributeOriginY)
@@ -234,7 +236,7 @@ extension NewsCell {
 
 extension NewsCell {
     
-    func getLabelSize(text: String, font: UIFont) -> CGSize {
+    private func getLabelSize(text: String, font: UIFont) -> CGSize {
         let maxWidth = bounds.width - inset * 2
         let textBlock = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
         let rect = text.boundingRect(with: textBlock, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
@@ -245,7 +247,7 @@ extension NewsCell {
         return size
     }
     
-    func getCellHeight() {
+    private func getCellHeight() {
         cellHeight = 2 * inset + 3 * insetBetweenObjects + avatar.frame.height + newsText.frame.height + attachedImage.frame.height + numberOfLikes.frame.height
     }
     
