a9fc33bf55d55972ace48fd0f6d402fafa4cfc55
KirillovE <evgeniy.kirillov@live.ru>
2018-05-16 21:08:30 +0300

    Проверяем наличие новых запросов на дружбу в фоне

--- newVK/AppDelegate.swift
+++ newVK/AppDelegate.swift
@@ -13,6 +13,15 @@ import FirebaseCore
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let fetchFriendsRequestsGroup = DispatchGroup()
+    var timer: DispatchSourceTimer?
+    var lastUpdate: Date? {
+        get { return UserDefaults.standard.object(forKey: "lastUpdate") as? Date }
+        set { UserDefaults.standard.setValue(newValue, forKey: "lastUpdate") }
+    }
+    let checkFriends = CheckNewFriendsRequest()
+    let checkFriendsInterval = 900.0           //проверка каждые 15 минут
+    let timeToHandleRequests = 10.0
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let config = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
@@ -20,6 +29,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         return true
+    }
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("Начинаем проверку наличия запросов от друзей в фоне. На часах ", Date())
+        if lastUpdate != nil, abs(lastUpdate!.timeIntervalSinceNow) < checkFriendsInterval {
+            print("Недавно проверяли, рано ещё, не надо так часто дёргать")
+            completionHandler(.noData)
+            return
+        }
+        
+        fetchFriendsRequestsGroup.enter()
+        checkFriends.makeRequest { requestsCount, potentialFriends in
+            print("Количество поступивших запросов: \(requestsCount)")
+            if requestsCount != 0 {
+                print("Идентификаторы потенциальных друзей: \(potentialFriends)")
+            }
+            self.fetchFriendsRequestsGroup.leave()
+        }
+        
+        fetchFriendsRequestsGroup.notify(queue: .main) {
+            print("Данные загружены в фоне")
+            self.timer = nil
+            self.lastUpdate = Date()
+            completionHandler(.newData)
+            return
+        }
+        
+        timer = DispatchSource.makeTimerSource(queue: .main)
+        timer?.schedule(deadline: .now() + timeToHandleRequests)
+        timer?.setEventHandler {
+            print("Не успели выполнить запрос")
+            self.fetchFriendsRequestsGroup.suspend()
+            completionHandler(.failed)
+            return
+        }
+        timer?.resume()
+        
     }
 
 }
