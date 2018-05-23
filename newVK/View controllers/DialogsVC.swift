b6e99abb24a2a87634e8cc5d4c15e21130804ace
KirillovE <evgeniy.kirillov@live.ru>
2018-05-23 12:06:46 +0300

    Создан класс для управления экраном диалогов

---
+++ newVK/View controllers/DialogsVC.swift
@@ -0,0 +1,15 @@
+//
+//  DialogsVC.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 23.05.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import UIKit
+
+class DialogsVC: UITableViewController {
+    
+    
+
+}
