1d62ed18c19e99a99ffd93070c624bc5f0f3dca0
KirillovE <evgeniy.kirillov@live.ru>
2018-05-23 12:47:54 +0300

    Создан класс для хранения сообщения

---
+++ newVK/Models from VK/Message.swift
@@ -0,0 +1,29 @@
+//
+//  Message.swift
+//  newVK
+//
+//  Created by Евгений Кириллов on 23.05.2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import SwiftyJSON
+
+class Message {
+    
+    let id: Int
+    let body: String
+    let userID: Int
+    let date: Double
+    let readState: Bool
+    let out: Bool
+    
+    init(json: JSON) {
+        id = json["id"].intValue
+        body = json["body"].stringValue
+        userID = json["user_id"].intValue
+        date = json["date"].doubleValue
+        readState = json["read_state"].boolValue
+        out = json["out"].boolValue
+    }
+    
+}
