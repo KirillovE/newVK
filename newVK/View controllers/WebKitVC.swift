c24f4dedefdc4876348a8f39ca36892b9655bc51
KirillovE <evgeniy.kirillov@live.ru>
2018-05-23 12:46:58 +0300

    Измнена версия API

--- newVK/View controllers/WebKitVC.swift
+++ newVK/View controllers/WebKitVC.swift
@@ -17,7 +17,7 @@ class WebKitVC: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     var sessionManager: SessionManager?
     let url = "https://oauth.vk.com/authorize"
-    let apiVersion = 5.74
+    let apiVersion = 5.76
     let clientID = 6356387
     let userDefaults = UserDefaults.standard
     let scope = "offline, photos, groups, wall, friends, messages"
